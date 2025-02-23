5bf19e2ab0362714cd8fdffdf42217839a47a873
inhardwetrust <vitalii.ivko1@gmail.com>
2025-02-23 16:38:30 +0100

    Real HS settings

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -29,9 +29,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_ll_usb.h \
  ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pcd_ex.h \
  ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_exti.h \
- C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/USB_DEVICE/App/usb_device.h \
- ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
- C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/USB_DEVICE/Target/usbd_conf.h
+ C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/LEG_USB/USB_DEVICE/App/usb_device.h \
+ ../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/LEG_USB/USB_DEVICE/Target/usbd_conf.h
 ../Core/Inc/main.h:
 ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal.h:
 ../Core/Inc/stm32u5xx_hal_conf.h:
@@ -63,6 +63,6 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_ll_usb.h:
 ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pcd_ex.h:
 ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_exti.h:
-C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/USB_DEVICE/App/usb_device.h:
-../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
-C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/USB_DEVICE/Target/usbd_conf.h:
+C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/LEG_USB/USB_DEVICE/App/usb_device.h:
+../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/LEG_USB/USB_DEVICE/Target/usbd_conf.h:
---
+++ Debug/LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.d
@@ -0,0 +1,75 @@
+LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.o: \
+ ../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c \
+ ../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/LEG_USB/USB_DEVICE/Target/usbd_conf.h \
+ ../Core/Inc/main.h ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal.h \
+ ../Core/Inc/stm32u5xx_hal_conf.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_rcc.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32U5xx/Include/stm32u5xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32U5xx/Include/stm32u5g9xx.h \
+ ../Drivers/CMSIS/Include/core_cm33.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv8.h \
+ ../Drivers/CMSIS/Device/ST/STM32U5xx/Include/system_stm32u5xx.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_rcc_ex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_gpio.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_gpio_ex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_icache.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_dma.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_dma_ex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_cortex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_flash.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_flash_ex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pwr.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pwr_ex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_tim.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_tim_ex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pcd.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_ll_usb.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pcd_ex.h \
+ ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_exti.h \
+ ../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ ../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ ../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ ../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
+../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+C:/Users/v.ivko/Documents/ivko_files/stm-workspace/u5g9_sbstack/LEG_USB/USB_DEVICE/Target/usbd_conf.h:
+../Core/Inc/main.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal.h:
+../Core/Inc/stm32u5xx_hal_conf.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_rcc.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32U5xx/Include/stm32u5xx.h:
+../Drivers/CMSIS/Device/ST/STM32U5xx/Include/stm32u5g9xx.h:
+../Drivers/CMSIS/Include/core_cm33.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv8.h:
+../Drivers/CMSIS/Device/ST/STM32U5xx/Include/system_stm32u5xx.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_rcc_ex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_gpio.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_gpio_ex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_icache.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_dma.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_dma_ex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_cortex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_flash.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_flash_ex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pwr.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pwr_ex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_tim.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_tim_ex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pcd.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_ll_usb.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_pcd_ex.h:
+../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_exti.h:
+../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+../LEG_USB/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
